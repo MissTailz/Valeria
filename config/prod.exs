41119916b17bd04c8459ec5bf1261eb5c17a6d13
Evelynn Harper <63874567+MissTailz@users.noreply.github.com>
2020-12-18 07:25:58 +0000

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -9,3 +9,5 @@ config :valeria, Valeria.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
+
+import_config "#{Mix.env()}.exs"
