31fd6d6b8f6acc6241fa4c4bd56e78be7224ace7
Evelynn Harper <63874567+MissTailz@users.noreply.github.com>
2020-12-18 08:06:40 +0000

    Update

--- config/config.exs
+++ config/config.exs
@@ -8,10 +8,8 @@ config :porcelain, driver: Porcelain.Driver.Basic
 
 config :valeria, Valeria.Repo,
   username: "postgres",
-  password: "@Sdftails",
+  password: "postgres",
   database: "Valeria",
   hostname: "127.0.0.1"
 
 config :valeria, ecto_repos: [Valeria.Repo]
-
-import_config "#{Mix.env()}.exs"
