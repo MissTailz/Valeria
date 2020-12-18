e2a57b3c978849e530512b65ab5f344cd3ccb7f3
Evelynn Harper <63874567+MissTailz@users.noreply.github.com>
2020-12-18 07:50:32 +0000

    7th times the charm

--- config/prod.exs
+++ config/prod.exs
@@ -1,12 +1,6 @@
 use Mix.Config
 
-config :nostrum,
-  token: System.get_env("TOKEN"),
-  num_shards: :auto
-
 config :valeria, Valeria.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-import_config "#{Mix.env()}.exs"
